96617fb09abdc020c05c15074247c37baa229ec7
AllenNotAlan <allenloyola23@gmail.com>
2021-04-07 11:33:30 +0100

    Log.h, Log.cpp added

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -1,7 +1,9 @@
 #pragma once
 
 //For use by Hazel applications
+#include <stdio.h>
 #include "Hazel/Application.h"
+#include "Hazel/Log.h"
 
 //--entry point------------
 #include "Hazel/EntryPoint.h"
--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -6,7 +6,13 @@ extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	Hazel::Log::Init();
+	HZ_CORE_WARN("Initialised log!");
+	int a = 5;
+	HZ_INFO("Hello! Var={0}", a);
+	HZ_CRITICAL("damnnn");
 
+	/*printf("Hazel Engine!");*/
 	auto app = Hazel::CreateApplication();
 	app->Run();
 	delete app;
